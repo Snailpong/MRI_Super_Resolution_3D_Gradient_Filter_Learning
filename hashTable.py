overflow fix

hashTable.py
@@ -54,7 +54,7 @@ def determine_geometric_func():
         strength_func, coherence_func = trace, fa
 
 # Quantization procedure to get the optimized strength and coherence boundaries
-@njit
+# @njit
 def quantization_border(im, im_GX, im_GY, im_GZ, patchNumber, w, quantization, instance):
     H, W, D = im_GX.shape
     for i1 in range(H - 2 * C.PATCH_HALF):
@@ -62,12 +62,11 @@ def quantization_border(im, im_GX, im_GY, im_GZ, patchNumber, w, quantization, i
         for j1 in range(W - 2 * C.PATCH_HALF):
             for k1 in range(D - 2 * C.PATCH_HALF):
 
-                if random.random() > 0.2 or im[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF] == 0:
+                if random.random() > 0.2 or np.any(im[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF] == 0):
                     continue
 
                 idx1 = (slice(i1+1, (i1 + 2 * C.GRADIENT_HALF + 2)), slice(j1+1, (j1 + 2 * C.GRADIENT_HALF + 2)),
                         slice(k1+1, (k1 + 2 * C.GRADIENT_HALF + 2)))
-                patch = im[idx1]
 
                 patchX = im_GX[idx1]
                 patchY = im_GY[idx1]
@@ -79,6 +78,15 @@ def quantization_border(im, im_GX, im_GY, im_GZ, patchNumber, w, quantization, i
                 patchNumber += 1
     return quantization, patchNumber
 
+@njit(nopython=True)
+def arrays_equal(a, b):
+    if a.shape != b.shape:
+        return False
+    for ai, bi in zip(a.flat, b.flat):
+        if ai != bi:
+            return False
+    return True
+
 @njit
 def get_hash(patchX, patchY, patchZ, weight, stre, cohe):
     G = np.vstack((patchX.ravel(), patchY.ravel(), patchZ.ravel())).T
@@ -86,8 +94,10 @@ def get_hash(patchX, patchY, patchZ, weight, stre, cohe):
     w, v = np.linalg.eig(x)
 
     index = w.argsort()[::-1]
-    [l1, l2, l3] = np.abs(w[index])
-    v = np.real(v[:, index])
+    # [l1, l2, l3] = np.abs(w[index])
+    # v = np.real(v[:, index])
+    [l1, l2, l3] = w[index]
+    v = v[:, index]
 
     angle_p = atan2(v[1, 0], v[0, 0])
     angle_t = acos(v[2, 0] / (sqrt((v[0, 0]) ** 2 + v[1, 0] ** 2 + v[2, 0] ** 2) + 1e-16))
@@ -128,8 +138,9 @@ def grad(patchX, patchY, patchZ, weight):
     return lamda, u
 
 def train_qv(im_LR, im_HR, im_GX, im_GY, im_GZ, w, stre, cohe, Q, V, mark):
-    for t in range(C.R ** 3):
-        Q, V, mark = train_qv_type(t, im_LR, im_HR, im_GX, im_GY, im_GZ, w, stre, cohe, Q, V, mark)
+    # for t in range(C.R ** 3):
+    #     Q, V, mark = train_qv_type(t, im_LR, im_HR, im_GX, im_GY, im_GZ, w, stre, cohe, Q, V, mark)
+    Q, V, mark = train_qv_type(0, im_LR, im_HR, im_GX, im_GY, im_GZ, w, stre, cohe, Q, V, mark)
     return Q, V, mark
 
 
@@ -138,63 +149,68 @@ def init_buckets(Q_TOTAL):
     xS = [[] for j in range(Q_TOTAL)]
     return patchS, xS
 
+def chunk(lst, size):
+    return list(map(lambda x: lst[x * size:x * size + size], list(range(0, ceil(len(lst) / size)))))
+
 
 def train_qv_type(t, im_LR, im_HR, im_GX, im_GY, im_GZ, w, stre, cohe, Q, V, mark):
     H, W, D = im_HR.shape
-    xd = (t // (C.R * C.R)) % C.R
-    yd = (t // C.R) % C.R
-    zd = t % C.R
 
-    patchS, xS = init_buckets(C.Q_ANGLE_P * C.Q_ANGLE_T * C.Q_STRENGTH * C.Q_COHERENCE)
-    timer = time.time()
+    xyz_range = [[x, y, z] for x in range(H - 2 * C.PATCH_HALF) for y in range(W - 2 * C.PATCH_HALF) for z in
+                 range(D - 2 * C.PATCH_HALF)]
+    sample_range = random.sample(xyz_range, len(xyz_range) // C.TRAIN_DIV)
+    point_list = chunk(sample_range, len(sample_range) // C.TRAIN_DIV + 1)
 
-    for i1 in range(xd, H - 2 * C.PATCH_HALF, C.R):
-        print('\r{} / {}    {} / {}    {} s'.format(t+1, C.R ** 3, i1 // C.R, (H - 2 * C.PATCH_HALF) // C.R, ((time.time() - timer) * 1000 // 10) / 100), end='')
+    for sample_idx, point_list1 in enumerate(point_list):
+        print('\r{} / {}'.format(sample_idx + 1, len(point_list)), end='', flush=True)
+        timer = time.time()
+        patchS, xS = init_buckets(C.Q_TOTAL)
+
+        for i1, j1, k1 in point_list1:
+
+            if np.any(im_HR[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF] == 0):
+                continue
+
+            idx1 = (slice(i1, (i1 + 2 * C.PATCH_HALF + 1)), slice(j1, (j1 + 2 * C.PATCH_HALF + 1)),
+                    slice(k1, (k1 + 2 * C.PATCH_HALF + 1)))
+
+            patch = im_LR[idx1]
+
+            idx2 = (slice(i1+1, (i1 + 2 * C.GRADIENT_HALF + 2)), slice(j1+1, (j1 + 2 * C.GRADIENT_HALF + 2)),
+                    slice(k1+1, (k1 + 2 * C.GRADIENT_HALF + 2)))
+
+            patchX = im_GX[idx2]
+            patchY = im_GY[idx2]
+            patchZ = im_GZ[idx2]
+
+            angle_p, angle_t, lamda, u = get_hash(patchX, patchY, patchZ, w, stre, cohe)
+            j = int(angle_p * C.Q_STRENGTH * C.Q_COHERENCE * C.Q_ANGLE_T + angle_t * C.Q_STRENGTH * C.Q_COHERENCE + lamda * C.Q_COHERENCE + u)
+
+            patch1 = patch.reshape(-1)
+            x1 = im_HR[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF]
+
+            patchS[j].append(patch1)
+            xS[j].append(x1)
+
+        print('\t{} s'.format(((time.time() - timer) * 1000 // 10) / 100), end='', flush=True)
         timer = time.time()
 
-        for j1 in range(yd, W - 2 * C.PATCH_HALF, C.R):
-            for k1 in range(zd, D - 2 * C.PATCH_HALF, C.R):
+        for j in range(C.Q_TOTAL):
+            if len(xS[j]) != 0:
+                A = cp.array(patchS[j])
+                b = cp.array(xS[j]).reshape(-1, 1)
+                Q += cp.dot(A.T, A)
+                V += cp.dot(A.T, b).reshape(-1)
+                # Qa = cp.array(Q[j])
+                # Va = cp.array(V[j])
+                #
+                # Qa += cp.dot(A.T, A)
+                # Va += cp.dot(A.T, b).reshape(-1)
+                #
+                # Q[j] = Qa.get()
+                # V[j] = Va.get()
 
-                if random.random() > 0.33 or im_HR[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF] == 0:
-                    continue
-
-                idx1 = (slice(i1, (i1 + 2 * C.PATCH_HALF + 1)), slice(j1, (j1 + 2 * C.PATCH_HALF + 1)),
-                        slice(k1, (k1 + 2 * C.PATCH_HALF + 1)))
-
-                patch = im_LR[idx1]
-
-                idx2 = (slice(i1+1, (i1 + 2 * C.GRADIENT_HALF + 2)), slice(j1+1, (j1 + 2 * C.GRADIENT_HALF + 2)),
-                        slice(k1+1, (k1 + 2 * C.GRADIENT_HALF + 2)))
-
-                patchX = im_GX[idx2]
-                patchY = im_GY[idx2]
-                patchZ = im_GZ[idx2]
-
-                angle_p, angle_t, lamda, u = get_hash(patchX, patchY, patchZ, w, stre, cohe)
-                j = int(angle_p * C.Q_STRENGTH * C.Q_COHERENCE * C.Q_ANGLE_T + angle_t * C.Q_STRENGTH * C.Q_COHERENCE + lamda * C.Q_COHERENCE + u)
-
-                patch1 = patch.reshape(-1)
-                x1 = im_HR[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF]
-
-                patchS[j].append(patch1)
-                xS[j].append(x1)
-
-    timer = time.time()
-
-    for j in range(C.Q_TOTAL):
-        if len(xS[j]) != 0:
-            A = cp.array(patchS[j])
-            b = cp.array(xS[j]).reshape(-1, 1)
-            Qa = cp.array(Q[j])
-            Va = cp.array(V[j])
-
-            Qa += cp.dot(A.T, A)
-            Va += cp.dot(A.T, b).reshape(-1)
-
-            Q[j] = Qa.get()
-            V[j] = Va.get()
-
-    print('   qv {} s'.format((time.time() - timer) * 100 // 10 / 10), end='')
+        print('\tqv {} s'.format((time.time() - timer) * 100 // 10 / 10), end='', flush=True)
 
     return Q, V, mark
 

test.py
@@ -22,11 +22,15 @@ from skimage.measure import compare_psnr
 C.argument_parse()
 determine_geometric_func()
 
+C.R = 4
 C.USE_PIXEL_TYPE = False
+# C.H_FILE = './arrays/h_3'
+
 
 def make_image(im_LR, im_GX, im_GY, im_GZ, w, stre, cohe, h):
     H = im_LR.shape[0]
     result_image = im_LR.copy()
+    # im_LR = np.array(im_LR, dtype=np.float64)
 
     timer = time.time()
     for i1 in range(H - 2 * PATCH_HALF):
@@ -38,17 +42,24 @@ def make_image(im_LR, im_GX, im_GY, im_GZ, w, stre, cohe, h):
 
     return result_image
 
+
 # @njit(parallel=True)
+@njit
 def make_image_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w, stre, cohe, h):
     H, W, D = im_LR.shape
-    for j1 in prange(W - 2 * PATCH_HALF):
-        for k1 in prange(D - 2 * PATCH_HALF):
+    for j1 in range(W - 2 * PATCH_HALF):
+        for k1 in range(D - 2 * PATCH_HALF):
             idx1 = (slice(i1, (i1 + 2 * PATCH_HALF + 1)), slice(j1, (j1 + 2 * PATCH_HALF + 1)),
                     slice(k1, (k1 + 2 * PATCH_HALF + 1)))
             patch = im_LR[idx1]
 
             if im_LR[i1 + C.PATCH_HALF, j1 + C.PATCH_HALF, k1 + C.PATCH_HALF] == 0:
-                    continue
+                continue
+
+            np.where(patch == 0, patch[PATCH_HALF, PATCH_HALF, PATCH_HALF], patch)
+
+            # if np.any(patch == 0):
+            #     patch[np.where(patch == 0)] = patch[PATCH_HALF, PATCH_HALF, PATCH_HALF]
 
             idx2 = (slice(i1+1, (i1 + 2 * GRADIENT_HALF + 2)), slice(j1+1, (j1 + 2 * GRADIENT_HALF + 2)),
                     slice(k1+1, (k1 + 2 * GRADIENT_HALF + 2)))
@@ -59,14 +70,17 @@ def make_image_yz(i1, result_image, im_LR, im_GX, im_GY, im_GZ, w, stre, cohe, h
 
             angle_p, angle_t, lamda, u = get_hash(patchX, patchY, patchZ, w, stre, cohe)
             j = int(angle_p * Q_STRENGTH * Q_COHERENCE * Q_ANGLE_T + angle_t * Q_STRENGTH * Q_COHERENCE + lamda * Q_COHERENCE + u)
-            t = (i1 % R) * R * R + (j1 % R) * R + k1 % R
 
             patch1 = patch.ravel()
             result_image[i1 + PATCH_HALF, j1 + PATCH_HALF, k1 + PATCH_HALF] = np.dot(patch1, h[j])
 
     return result_image
 
+
 current_hour = time.strftime('%m%d%H', time.localtime(time.time()))
+result_dir = './result/{}/'.format(current_hour)
+if not os.path.isdir(result_dir):
+    os.mkdir(result_dir)
 # current_hour = '080413'
 
 testPath = './test'
@@ -75,65 +89,66 @@ file_list = make_dataset(testPath)
 # Preprocessing normalized Gaussian matrix W for hashkey calculation
 G_WEIGHT = get_normalized_gaussian()
 
-h = np.load('{}.npy'.format(C.H_FILE))
+h = np.load('./arrays/h_{}.npy'.format(C.R))
 h = np.array(h, dtype=np.float32)
 
-with open("./arrays/Qfactor_str" + str(R), "rb") as sp:
+with open("./arrays/Qfactor_str" + str(C.R), "rb") as sp:
     stre = pickle.load(sp)
 
-with open("./arrays/Qfactor_coh" + str(R), "rb") as cp:
+with open("./arrays/Qfactor_coh" + str(C.R), "rb") as cp:
     cohe = pickle.load(cp)
 
-for file_idx, file in enumerate(file_list):
-    filestart = time.time()
+print(stre, cohe)
+filestart = time.time()
 
+for file_idx, file in enumerate(file_list):
     file_name = file.split('\\')[-1].split('.')[0]
     print('\r', end='')
     print('' * 60, end='')
-    print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')')
+    print('\rProcessing ' + str(file_idx + 1) + '/' + str(len(file_list)) + ' image (' + file_name + ')' + str(time.time() - filestart))
+    filestart = time.time()
 
     raw_image = nib.load(file).dataobj
-    crop_image = mod_crop(raw_image, R)
+    crop_image = mod_crop(raw_image, C.R)
     clipped_image = clip_image(crop_image)
-    slice_area = crop_slice(clipped_image, PATCH_SIZE // 2, R)
+    slice_area = crop_slice(clipped_image, PATCH_SIZE // 2, C.R)
 
     im_blank_LR = get_lr(clipped_image) / clipped_image.max()  # Prepare the cheap-upscaling images
     im_LR = im_blank_LR[slice_area]
     im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
+
+    im_GX[np.where(im_LR == 0)] = 0
+    im_GY[np.where(im_LR == 0)] = 0
+    im_GZ[np.where(im_LR == 0)] = 0
+
     im_HR = clipped_image[slice_area] / clipped_image.max()
 
     im_result = make_image(im_LR, im_GX, im_GY, im_GZ, G_WEIGHT, stre, cohe, h)
     # im_blending = Blending2(im_LR, im_result)
-    im_blending = blend_image3(im_LR, im_result, 5)
+    # im_blending = blend_image(im_LR, im_result)
+    # im_blending = blend_image3(im_LR, im_result, 3.5)
     # im_blending = Backprojection(imL, im_blending, 50) #Optional: Backprojection, which can slightly improve PSNR, especilly for large upscaling factor.
-    im_blending = np.clip(im_blending, 0, 1)
+    # im_blending = np.clip(im_blending, 0, 1)
 
     output_img = np.zeros(raw_image.shape)
     output_img[slice_area] = im_result
     output_img = output_img * clipped_image.max()
     ni_img = nib.Nifti1Image(output_img, np.eye(4))
-    nib.save(ni_img, './result/{}_{}_result.nii.gz'.format(file_name, current_hour))
+    nib.save(ni_img, '{}/{}_result.nii.gz'.format(result_dir, file_name))
 
-    output_img2 = np.zeros(raw_image.shape)
-    output_img2[slice_area] = im_blending
-    output_img2 = output_img2 * clipped_image.max()
-    ni_img2 = nib.Nifti1Image(output_img2, np.eye(4))
-    nib.save(ni_img2, './result/{}_{}_result_blend.nii.gz'.format(file_name, current_hour))
+    # output_img2 = np.zeros(raw_image.shape)
+    # output_img2[slice_area] = im_blending
+    # output_img2 = output_img2 * clipped_image.max()
+    # ni_img2 = nib.Nifti1Image(output_img2, np.eye(4))
+    # nib.save(ni_img2, '{}/{}_{}_result_blend.nii.gz'.format(result_dir, file_name, current_hour))
 
-    print(compare_psnr(im_HR, im_LR))
-    print(compare_psnr(im_HR, im_result))
-    print(compare_psnr(im_HR, im_blending))
-
+    print()
+    print(compare_psnr(im_HR, im_LR), compare_psnr(im_HR, im_result))
     area = np.nonzero(im_HR)
+    print(compare_psnr(im_HR[area], im_LR[area]), compare_psnr(im_HR[area], im_result[area]))
 
-    print(compare_psnr(im_HR[area], im_LR[area]))
-    print(compare_psnr(im_HR[area], im_result[area]))
-    print(compare_psnr(im_HR[area], im_blending[area]))
-
-
-
-    if file_idx == 0:
-        break
+    # if file_idx == 0:
+    #     break
 
 
 

train.py
@@ -19,6 +19,7 @@ from util import *
 C.argument_parse()
 determine_geometric_func()
 
+C.R = 4
 C.USE_PIXEL_TYPE = False
 
 Q, V, finished_files = load_files()
@@ -35,44 +36,51 @@ C.TRAIN_FILE_MAX = min(C.TRAIN_FILE_MAX, len(file_list))
 # Preprocessing normalized Gaussian matrix W for hashkey calculation
 G_WEIGHT = get_normalized_gaussian()
 
-# instance = 4000000                          # use 20000000 patches to get the Strength and coherence boundary
-# patchNumber = 0                              # patch number has been used
-# quantization = np.zeros((instance, 2))        # quantization boundary
-# for file_idx, image in enumerate(file_list):
-#     print('\r', end='')
-#     print('' * 60, end='')
-#     print('\r Quantization: Processing '+ image.split('\\')[-1] + str(instance) + ' patches (' + str(100*patchNumber/instance) + '%)')
-#
-#     raw_image = nib.load(image).dataobj
-#     crop_image = mod_crop(raw_image, C.R)
-#     clipped_image = clip_image(crop_image)
-#     slice_area = crop_slice(clipped_image, C.PATCH_SIZE // 2, C.R)
-#
-#     im_LR = get_lr(clipped_image)         # Prepare the cheap-upscaling images (optional: JPEG compression)
-#
-#     im_blank_LR = get_lr(clipped_image) / clipped_image.max()  # Prepare the cheap-upscaling images
-#     im_LR = im_blank_LR[slice_area]
-#     im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
-#
-#     quantization, patchNumber = quantization_border(im_LR, im_GX, im_GY, im_GZ, patchNumber, G_WEIGHT, quantization, instance)  # get the strength and coherence of each patch
-#     if patchNumber > instance / 2:
-#         break
-#
-# # uniform quantization of patches, get the optimized strength and coherence boundaries
-# quantization = quantization[0:patchNumber, :]
-# quantization = np.sort(quantization, axis=0)
-# for i in range(C.Q_STRENGTH - 1):
-#     stre[i] = quantization[floor((i+1) * patchNumber / C.Q_STRENGTH), 0]
-# for i in range(C.Q_COHERENCE - 1):
-#     cohe[i] = quantization[floor((i+1) * patchNumber / C.Q_COHERENCE), 1]
+instance = 5000000                          # use 20000000 patches to get the Strength and coherence boundary
+patchNumber = 0                              # patch number has been used
+quantization = np.zeros((instance, 2))        # quantization boundary
+for file_idx, image in enumerate(file_list):
+    print('\r', end='')
+    print('' * 60, end='')
+    print('\r Quantization: Processing '+ image.split('\\')[-1] + str(instance) + ' patches (' + str(100*patchNumber/instance) + '%)')
 
-stre[0] = 0.00075061
-stre[1] = 0.00297238
-cohe[0] = 0.42785409
-cohe[1] = 0.61220482
+    raw_image = nib.load(image).dataobj
+    crop_image = mod_crop(raw_image, C.R)
+    clipped_image = clip_image(crop_image)
+    slice_area = crop_slice(clipped_image, C.PATCH_SIZE // 2, C.R)
+
+    im_LR = get_lr(clipped_image)         # Prepare the cheap-upscaling images (optional: JPEG compression)
+
+    im_blank_LR = get_lr(clipped_image) / clipped_image.max()  # Prepare the cheap-upscaling images
+    im_LR = im_blank_LR[slice_area]
+    im_GX, im_GY, im_GZ = np.gradient(im_LR)  # Calculate the gradient images
+
+    quantization, patchNumber = quantization_border(im_LR, im_GX, im_GY, im_GZ, patchNumber, G_WEIGHT, quantization, instance)  # get the strength and coherence of each patch
+    if patchNumber > instance / 2:
+        break
+
+# uniform quantization of patches, get the optimized strength and coherence boundaries
+quantization = quantization[0:patchNumber, :]
+quantization = np.sort(quantization, axis=0)
+for i in range(C.Q_STRENGTH - 1):
+    stre[i] = quantization[floor((i+1) * patchNumber / C.Q_STRENGTH), 0]
+for i in range(C.Q_COHERENCE - 1):
+    cohe[i] = quantization[floor((i+1) * patchNumber / C.Q_COHERENCE), 1]
+
+# stre[0] = 0.00075061
+# stre[1] = 0.00297238
+# cohe[0] = 0.42785409
+# cohe[1] = 0.61220482
+
+# stre[0] = 0.0010374
+# stre[1] = 0.0031624
+# cohe[0] = 0.44441757
+# cohe[1] = 0.62840797
 
 print(stre, cohe)
 
+Q = cp.array(Q)
+V = cp.array(V)
 
 
 start = time.time()
@@ -97,14 +105,18 @@ for file_idx, file in enumerate(file_list):
     im_HR = clipped_image[slice_area] / clipped_image.max()
 
     Q, V, mark = train_qv(im_LR, im_HR, im_GX, im_GY, im_GZ, G_WEIGHT, stre, cohe, Q, V, mark)  # get Q, V of each patch
-
-    if file_idx + 1 == 100:
-        break
     
-    print(' ' * 23, 'last', '%.1f' % ((time.time() - filestart) / 60), 'min', end='', flush=True)
+    print(' ' * 30, 'last', '%.1f' % ((time.time() - filestart) / 60), 'min', end='', flush=True)
 
     finished_files.append(file)
-    ask_save_qv(Q, V, finished_files)
+
+    if file_idx + 1 == 25:
+        break
+
+    # ask_save_qv(Q, V, finished_files)
+
+Q = Q.get()
+V = V.get()
 
 save_qv(Q, V, finished_files)
 compute_h(Q, V)

util.py
@@ -42,8 +42,8 @@ def load_files():
         QVF.close()
         print('Done', flush=True)
     else:
-        Q = np.zeros((C.Q_TOTAL, C.FILTER_VOL, C.FILTER_VOL))
-        V = np.zeros((C.Q_TOTAL, C.FILTER_VOL))
+        Q = np.zeros((C.Q_TOTAL, C.FILTER_VOL, C.FILTER_VOL), dtype=np.float64)
+        V = np.zeros((C.Q_TOTAL, C.FILTER_VOL), dtype=np.float64)
         finished_files = []
 
     return Q, V, finished_files